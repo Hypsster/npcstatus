6a815dae377f7f2a1923da8c5562186e51c7ba3a
Hypsster <work@hypsster.com>
2023-03-02 15:17:23 -0500

    Modified File

---
+++ src/main/java/com/npcstatus/NpcStatus.java
@@ -0,0 +1,55 @@
+package com.npcstatus;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+        name = "NPC Statis",
+        enabledByDefault = false
+
+)
+public class NpcStatus extends Plugin
+{
+    @Inject
+    private Client client;
+
+    @Inject
+    private NpcStatusConfig config;
+
+    @Override
+    protected void startUp() throws Exception
+    {
+        log.info("Example started!");
+    }
+
+    @Override
+    protected void shutDown() throws Exception
+    {
+        log.info("Example stopped!");
+    }
+
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged gameStateChanged)
+    {
+        if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+        {
+            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+        }
+    }
+
+    @Provides
+    NpcStatusConfig provideConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(NpcStatusConfig.class);
+    }
+}
